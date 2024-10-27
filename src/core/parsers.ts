013ee0094aa227897f44f738784ed31493f96691
nmhillusion <nguyenminhhieu.geek@gmail.com>
2024-10-27 10:33:44 +0700

    refactor function

--- src/core/parsers.ts
+++ src/core/parsers.ts
@@ -5,28 +5,73 @@ import { IndexNode, NodeMetadata } from "../model/node.model";
 
 export const METADATA_REGEXP_PATTERN = /\bmeta(data)?\.ya?ml$/i;
 
-function parseMetadata(rawData: unknown, path_: string) : NodeMetadata {
-  const tags_ = (rawData["tags"] as string)?.split("\,|\;").map(s => s.trim()).filter(Boolean);
+function parseMetadata(rawData: unknown, path_: string): NodeMetadata {
+  const tags_ = (rawData["tags"] as string)
+    ?.split(",|;")
+    .map((s) => s.trim())
+    .filter(Boolean);
   const parsedPath = path.parse(path_);
-  
+
   return {
     author: rawData["author"],
     bannerPath: rawData["bannerPath"],
     tags: tags_ || [],
     publishDate: rawData["publishDate"],
     summary: rawData["summary"] || "",
-    title: rawData["title"] || parsedPath.name || parsedPath.dir
-  }
+    title: rawData["title"] || parsedPath.name || parsedPath.dir,
+  };
 }
 
-
-export function parseYamlToIndexNode(path_: string) : IndexNode {
+export function parseYamlToIndexNode(path_: string): IndexNode {
   const content_ = fs.readFileSync(path_, "utf8").toString();
   const rawData = jsYaml.load(content_);
   return {
     path: path_,
     metadata: parseMetadata(rawData, path_),
     rawData,
-    children: []
+    children: [],
   };
+}
+
+export function parseWalkThroughDir(startPoint: string) {
+  let currentNode: IndexNode = null;
+  let children: IndexNode[] = [];
+
+  if (!fs.lstatSync(startPoint).isDirectory()) {
+    throw Error("Entry point must be a directory");
+  }
+
+  const childrenResult = fs
+    .readdirSync(startPoint)
+    .map((it) => path.join(startPoint, it));
+
+  for (const itemPath_ of childrenResult) {
+    if (fs.lstatSync(itemPath_).isDirectory()) {
+      children.push(this.walkThroughDir(itemPath_));
+    } else if (path.basename(itemPath_).match(METADATA_REGEXP_PATTERN)) {
+      currentNode = parseYamlToIndexNode(itemPath_);
+    }
+  }
+
+  if (!currentNode) {
+    const parsedPath = path.parse(startPoint);
+
+    currentNode = {
+      path: startPoint,
+      children: null,
+      metadata: {
+        author: "",
+        bannerPath: "",
+        publishDate: "",
+        summary: "",
+        tags: [],
+        title: parsedPath.name || parsedPath.dir,
+      },
+      rawData: null,
+    };
+  }
+
+  currentNode.children = children;
+
+  return currentNode;
 }
--- src/indexity.ts
+++ src/indexity.ts
@@ -1,8 +1,7 @@
-import * as fs from "fs";
-import * as path from "path";
 import { IndexityAdapter } from "./core/adapter";
-import { IndexNode } from "./model/node.model";
-import { METADATA_REGEXP_PATTERN, parseYamlToIndexNode } from "./core/parsers";
+import {
+  parseWalkThroughDir
+} from "./core/parsers";
 
 export interface IndexityOptions {
   srcDir?: string;
@@ -23,51 +22,8 @@ export class Indexity {
     return this;
   }
 
-  private walkThroughDir(startPoint: string) {
-    let currentNode: IndexNode = null;
-    let children: IndexNode[] = [];
-
-    if (!fs.lstatSync(startPoint).isDirectory()) {
-      throw Error("Entry point must be a directory");
-    }
-
-    const childrenResult = fs
-      .readdirSync(startPoint)
-      .map((it) => path.join(startPoint, it));
-
-    for (const itemPath_ of childrenResult) {
-      if (fs.lstatSync(itemPath_).isDirectory()) {
-        children.push(this.walkThroughDir(itemPath_));
-      } else if (path.basename(itemPath_).match(METADATA_REGEXP_PATTERN)) {
-        currentNode = parseYamlToIndexNode(itemPath_);
-      }
-    }
-
-    if (!currentNode) {
-      const parsedPath = path.parse(startPoint);
-
-      currentNode = {
-        path: startPoint,
-        children: null,
-        metadata: {
-          author: "",
-          bannerPath: "",
-          publishDate: "",
-          summary: "",
-          tags: [],
-          title: parsedPath.name || parsedPath.dir,
-        },
-        rawData: null,
-      };
-    }
-
-    currentNode.children = children;
-
-    return currentNode;
-  }
-
   public async build() {
-    const metadata_ = this.walkThroughDir(this.options.srcDir);
+    const metadata_ = parseWalkThroughDir(this.options.srcDir);
 
     return {
       metadata: metadata_,
